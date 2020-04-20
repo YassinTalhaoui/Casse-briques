84d25af72545fee614e590f544bb66018c98e6e6
unknown <54892@etu.he2b.be>
2020-04-20 14:32:50 +0200

    Etape 1.3: Une raquette qui bouge manuellement.

--- Script/model/Paddle.js
+++ Script/model/Paddle.js
@@ -1,13 +1,28 @@
+/**
+ * This class represents the paddle of the game.
+ */
 class Paddle {
 
+    /**
+     * Constructor of Paddle.
+     * @param {*} left an integer.
+     */
     constructor(left) {
         this._left = left;
     }
 
+    /**
+     * Simple getter of _left.
+     */
     getLeft() {
         return this._left;
     }
 
+    /**
+     * Returns the new position of the racket in relation to
+     * the left edge of the scene. 
+     * @param {*} val an integer.
+     */
     moveTo(val) {
         this._left = val;
         return this._left;
--- Script/view/View.js
+++ Script/view/View.js
@@ -1,6 +1,13 @@
-
+/**
+ * Regroups any view details. 
+ */
 class View {
 
+   /**
+    * Modifies the css according to the left of the received paddle in order to
+    * that the racket is positioned in the right place.
+    * @param {*} paddle the Paddle.
+    */
    displayPaddle(paddle) {
       document.getElementById("raquette").style.marginLeft = paddle.getLeft() + "px";
 
